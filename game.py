e7dd5f2e02849ef125294c7ea060fac4444df0e4
Lari Löfman <larwazor@gmail.com>
2018-11-29 22:35:26 +0200

    Change Class 'Grid' to 'Map' and modify map1

--- game.py
+++ game.py
@@ -12,20 +12,20 @@ root = None
 screen = None
 embed = None
 menu_bar = None
-current_grid = None
+current_map = None
 
 
-class Grid():
-    """Handles drawing grid based maps."""
+class Map():
+    """Handles drawing map and interfaces the map data."""
 
     def __init__(self, map_name, cell_size):
-        global current_grid
+        global current_map
         self.map_name = map_name
         self.cell_size = cell_size
         self.map = maps.MapData(map_name)
         self.height = len(self.map.data)
         self.width = len(self.map.data[0])
-        self.set_current_grid()
+        self.set_current_map()
         self.draw_map()
         self.character_list = []
 
@@ -49,18 +49,18 @@ class Grid():
                     160, 160, 160)
                 pygame.draw.rect(screen, cell_color, rect)
 
-    def set_current_grid(self):
-        """Set self as current grid and clear old one's character list."""
-        global current_grid
+    def set_current_map(self):
+        """Set self as current map and clear old one's character list."""
+        global current_map
         try:
-            for char in current_grid.character_list:
+            for char in current_map.character_list:
                 del char
         except Exception:
             pass
-        current_grid = self
+        current_map = self
 
     def contains_position(self, pos):
-        """Test if position is on the grid"""
+        """Test if position is on the map"""
         if pos[0] >= 0 and pos[1] >= 0 and pos[0] < self.width and pos[1] < self.height:
             return True
         else:
@@ -75,8 +75,8 @@ class Grid():
 
     def can_move_to_position(self, pos):
         """Test if position is walkable, a '.'"""
-        pos_on_grid = self.get_pos(pos)
-        if pos_on_grid == '.':
+        pos_on_map = self.get_pos(pos)
+        if pos_on_map == '.':
             return True
         else:
             return False
@@ -87,25 +87,25 @@ def draw_circle():
     pygame.draw.circle(screen, (127, 63, 191), (250, 250), 125)
 
 
-def draw_grid(map_name, cell_size):
-    """Function to create a test grid."""
-    grid = Grid(map_name, cell_size)
+def draw_map(map_name, cell_size):
+    """Function to create a test map."""
+    map = Map(map_name, cell_size)
     add_character()
 
 
 def mark_positions(pos_list):
     """Mark list specified positions with a red circle"""
     for pos in pos_list:
-        x_pos = pos[0]*current_grid.cell_size + current_grid.cell_size // 2
-        y_pos = pos[1] * current_grid.cell_size + current_grid.cell_size // 2
-        size = current_grid.cell_size // 8
+        x_pos = pos[0]*current_map.cell_size + current_map.cell_size // 2
+        y_pos = pos[1] * current_map.cell_size + current_map.cell_size // 2
+        size = current_map.cell_size // 8
         pygame.draw.circle(screen, (255, 0, 0, 100), (x_pos, y_pos), size)
 
 
 def add_character():
-    global current_grid
+    global current_map
     global character_list
-    char = Character('wizard.png', (1, 1), current_grid)
+    char = Character('wizard.png', (1, 1), current_map)
 
 
 def create_window(window_width, window_height):
@@ -133,11 +133,11 @@ def create_menu_bar():
     options_menu = tk.Menu(menu_bar, tearoff=0)
     options_menu.add_command(label="Draw Circle", command=draw_circle)
     options_menu.add_command(
-        label="Create Map1, Cell size: 16x16", command=lambda: draw_grid('map1', 16))
+        label="Create Map1, Cell size: 16x16", command=lambda: draw_map('map1', 16))
     options_menu.add_command(
-        label="Create Map2, Cell size: 32x32", command=lambda: draw_grid('map2', 32))
+        label="Create Map2, Cell size: 32x32", command=lambda: draw_map('map2', 32))
     options_menu.add_command(
-        label="Create Map3, Cell size: 64x64", command=lambda: draw_grid('map3', 64))
+        label="Create Map3, Cell size: 64x64", command=lambda: draw_map('map3', 64))
     options_menu.add_separator()
     options_menu.add_command(label="Exit", command=root.quit)
     menu_bar.add_cascade(label="Options", menu=options_menu)
@@ -153,9 +153,8 @@ def set_sdl():
 def create_screen(screen_width, screen_height):
     """Create Pygame screen."""
     global screen
-    screen = pygame.display.set_mode((screen_width, screen_width))
+    screen = pygame.display.set_mode((screen_width, screen_height))
     screen.fill(pygame.Color(31, 31, 31))
-    pygame.display.init()
 
 
 # Initialize
@@ -174,33 +173,34 @@ def get_kb_input():
     kb_input = pygame.key.get_pressed()
     try:
         if kb_input[pygame.K_a]:
-            current_grid.character_list[0].move_to_direction(
+            current_map.character_list[0].move_to_direction(
                 'w', clear_sequence=True)
         elif kb_input[pygame.K_d]:
-            current_grid.character_list[0].move_to_direction(
+            current_map.character_list[0].move_to_direction(
                 'e', clear_sequence=True)
         elif kb_input[pygame.K_w]:
-            current_grid.character_list[0].move_to_direction(
+            current_map.character_list[0].move_to_direction(
                 'n', clear_sequence=True)
         elif kb_input[pygame.K_s]:
-            current_grid.character_list[0].move_to_direction(
+            current_map.character_list[0].move_to_direction(
                 's', clear_sequence=True)
         elif kb_input[pygame.K_g]:
-            current_grid.character_list[0].set_sequence(
+            current_map.character_list[0].set_sequence(
                 ((2, 1), (3, 1), (4, 1), (4, 2), (5, 2), (5, 3), (6, 3), (7, 3)))
         elif kb_input[pygame.K_j]:
-            print(current_grid.character_list[0].pos)
+            print(current_map.character_list[0].pos)
     except AttributeError:
         pass
 
 
 def mouse_on_window():
-
+    global embed
     mouse_x = root.winfo_pointerx() - root.winfo_rootx()
     mouse_y = root.winfo_pointery() - root.winfo_rooty()
-
     if mouse_x >= 0 and mouse_y >= 0:
         if mouse_x <= root.winfo_width() and mouse_y <= root.winfo_height():
+            # pygame.event.post(pygame.event.Event(
+            #    6, {'pos': (230, 122), 'button': 1}))
             return True
     return False
 
@@ -208,20 +208,20 @@ def mouse_on_window():
 def get_mouse_input():
     if mouse_on_window():
         for event in pygame.event.get():
-            if event.type == pygame.MOUSEBUTTONUP:
+            if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+                # print(event)
                 try:
                     mouse_pos = (pygame.mouse.get_pos()[
-                        0] // current_grid.cell_size, pygame.mouse.get_pos()[1] // current_grid.cell_size)
+                        0] // current_map.cell_size, pygame.mouse.get_pos()[1] // current_map.cell_size)
                     flash_pos(mouse_pos)
-                    print('click')
                 except AttributeError:
                     pass
 
 
 def flash_pos(pos):
-    flash_size = current_grid.cell_size
+    flash_size = current_map.cell_size
     flash_color = (255, 0, 0)
-    current_grid.get_pos(pos)
+    current_map.get_pos(pos)
     rect = pygame.Rect(pos[0]*flash_size, pos[1] *
                        flash_size, flash_size, flash_size)
 
@@ -233,8 +233,8 @@ def game_loop():
     global root
     update_interval = 16
     try:
-        current_grid.draw()
-        for char in current_grid.character_list:
+        current_map.draw()
+        for char in current_map.character_list:
             char.move(update_interval)
             char.draw(screen)
             mark_positions(char.move_sequence)
