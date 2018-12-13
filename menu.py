dc2e4db1b030131387d0cf23a775bc0407b6e3e8
Lari Löfman <larwazor@gmail.com>
2018-12-14 00:37:29 +0200

    Add functional menu system

--- menu.py
+++ menu.py
@@ -3,7 +3,7 @@ from functools import partial
 
 
 class MenuBar():
-    """Top menu bar"""
+    """Menu bar with menu objects as children"""
 
     def __init__(self, screen, color=[0, 80, 60], pos=[0, 0], size=[512, 32]):
         self.screen = screen
@@ -14,6 +14,7 @@ class MenuBar():
         self.open_menu = None
         self.hovered_menu = None
         self.hover_child = None
+        self.cached_hover = None
         pygame.font.init()
 
     def update(self):
@@ -22,19 +23,16 @@ class MenuBar():
         self.draw()
 
     def check_hover(self):
-
-        mouse_pos = pygame.mouse.get_pos()
+        """Find a child that has mouse over itself"""
 
         # When mouse is outside window, reset hover child
         if not pygame.mouse.get_focused():
-
             if self.hover_child:
                 self.hover_child.hover = False
-
-            self.hover_child = None
-
-            self.close_menus()
+                self.hover_child = None
             return
+
+        mouse_pos = pygame.mouse.get_pos()
 
         # Find child under cursor
         for child in self.children:
@@ -42,11 +40,9 @@ class MenuBar():
                 if mouse_pos[1] > child.pos[1] and mouse_pos[1] < child.pos[1] + child.size[1]:
                     self.hover_child = child
                     child.hover = True
-
-                    # if child.children:
-                    #     self.open_menu = child
-                    # else:
-                    #     self.open_menu = child.parent
+                    # If a menu has been clicked open but cursor is over another menu, open that one with a click
+                    if self.open_menu and self.open_menu != child and child.children:
+                        child.get_clicked()
                     continue
             child.hover = False
 
@@ -59,23 +55,27 @@ class MenuBar():
                          (self.pos[0], self.pos[1], self.size[0], self.size[1]), 0)
 
         for child in self.children:
-            child.draw()
+            if child.is_active:
+                child.draw()
 
     def process_click(self):
-
+        """Calls click on a hovered child or closes all menus if click is outside menu items"""
         for child in self.children:
             if child.hover:
                 child.get_clicked()
-                break
+                if child.is_active:
+                    break
         else:
             self.close_menus()
 
     def close_menus(self):
+        """Close the currently open menu"""
         if self.open_menu:
             self.open_menu.close()
             self.open_menu = None
 
     def add_menu(self, text='', command=None):
+        """Creates a menu and sorts it horizontally with existing ones"""
         menu_size = (int(self.size[0] / 4), self.size[1])
         menu = Menu(self, self.screen, (100, 100, 20),
                     self.pos.copy(), menu_size, text=text, highlight_color=(150, 150, 70), click_color=(200, 200, 100), command=command)
@@ -87,7 +87,7 @@ class MenuBar():
 
 
 class Menu():
-    """Menu parent"""
+    """Menu parent object with menu items as children"""
 
     def __init__(self, parent, screen, color, pos, size, text='', highlight_color=None, click_color=None, command=None):
         self.parent = parent
@@ -104,6 +104,7 @@ class Menu():
         self.click_timer = self.flash_duration
         self.children = []
         self.hover = False
+        self.is_active = True
 
     def draw(self):
         """Draws self and calls draw on children"""
@@ -124,10 +125,6 @@ class Menu():
         pygame.draw.rect(self.screen, (self.color),
                          (self.pos[0], self.pos[1], self.size[0], self.size[1]), 0)
 
-        for child in self.children:
-            if child.is_active:
-                child.draw()
-
         if self.text:
             font = pygame.font.SysFont(
                 'Verdana, Segoe UI, Arial', int(self.size[1] * 0.75))
@@ -136,7 +133,9 @@ class Menu():
                                     self.pos[1] + (self.size[1]/2 - text.get_height()/2)))
 
     def get_clicked(self):
+        """Flash button and call the command"""
 
+        # If there is an open menu and it is not self, make self the open menu, otherwise close self
         if self.parent.open_menu:
             if self.parent.open_menu != self:
                 self.parent.open_menu.close()
@@ -154,16 +153,19 @@ class Menu():
                 partial(self.command)()
 
     def open(self):
+        """Mark self as the open menu in parent and activate all children"""
         self.parent.open_menu = self
         for child in self.children:
             child.is_active = True
 
     def close(self):
+        """Remove parent's open menu and deactivate all children"""
         self.parent.open_menu = None
         for child in self.children:
             child.is_active = False
 
     def add_item(self, text='', command=None):
+        """Adds a menu item and sorts it vertically with others"""
         item = MenuItem(self, self.screen, self.color, self.pos.copy(),
                         self.size, text=text, highlight_color=self.highlight_color, command=command)
 
@@ -182,6 +184,7 @@ class MenuItem(Menu):
         self.is_active = False
 
     def get_clicked(self):
+        """Calls command and closes parent menu"""
 
         if not self.is_active:
             return
@@ -197,9 +200,6 @@ class MenuItem(Menu):
     def draw(self):
         """Draws self"""
 
-        if not self.screen or not self.is_active:
-            return
-
         if self.hover:
             self.color = self.highlight_color
         else:
@@ -214,150 +214,3 @@ class MenuItem(Menu):
             text = font.render(self.text, 1, (0, 0, 0))
             self.screen.blit(text, (self.pos[0] + (self.size[0]/2 - text.get_width()/2),
                                     self.pos[1] + (self.size[1]/2 - text.get_height()/2)))
-
-
-# class Menu():
-#     """Menu parent"""
-
-#     active_menu = []
-
-#     def __init__(self, screen, color, pos, size):
-#         self.screen = screen
-#         self.color = color
-#         self.x = pos[0]
-#         self.y = pos[1]
-#         self.width = size[0]
-#         self.height = size[1]
-#         self.children = []
-
-#     def process_click(self):
-#         for child in self.children:
-#             child.get_clicked()
-
-#     def draw(self):
-#         """Draws menu object on screen and calls draw on its children objects"""
-#         if not self.screen:
-#             return
-
-#         pygame.draw.rect(self.screen, self.color,
-#                          (self.x, self.y, self.width, self.height), 0)
-
-#         for child in self.children:
-#             child.draw()
-
-#     def add_menu_button(self, color=(208, 208, 208), size=(128, 32), text='', highlight_color=(240, 240, 240), click_color=(255, 255, 255), command=None, h_layout=True):
-#         """Adds menu button as a child"""
-#         button = MenuButton(self.screen, color,
-#                             (self.x, self.y), size, text=text, highlight_color=highlight_color, click_color=click_color, command=command, h_layout=h_layout, parent=self)
-#         self.children.append(button)
-
-#         # Sort children vertically or horizontally based on h_layout
-#         for index, child in enumerate(self.children):
-#             if h_layout:
-#                 child.x = self.x + (child.width * index)
-#                 child.is_active = True
-#             else:
-#                 child.y = self.y + (child.height * (index + 1))
-#         return button
-
-
-# class MenuButton(Menu):
-#     """Menu button
-
-#         **command accepts a callable or a callable and a parameter as a list
-#     """
-
-#     def __init__(self, screen, color, pos, size, text='', highlight_color=None, click_color=None, command=None, h_layout=True, parent=None):
-#         super().__init__(screen, color, pos, size)
-#         self.text = text
-#         self.orig_color = color
-#         self.highlight_color = highlight_color if highlight_color != None else color
-#         self.click_color = click_color if click_color != None else color
-#         self.command = command
-#         self.hover = False  # Is mouse over self
-#         self.flash_duration = 5  # How many screen refreshes is the click color shown
-#         self.click_timer = self.flash_duration
-#         self.h_layout = h_layout  # Whether children are ordered horizontally or vertically
-#         self.is_active = False  # Should we be shown
-#         self.parent = parent
-#         pygame.font.init()
-
-#     def get_clicked(self):
-#         """Calls command with possible argument"""
-
-#         if self.hover:
-
-#             if self not in Menu.active_menu:
-#                 self.close_menu()
-#             # If we are last child in menu tree, self and siblings inactive
-#             if not self.children:
-#                 self.close_menu()
-#             # If we are top menu button, set children active
-#             else:
-#                 self.open_menu()
-#             # Call command if we have one
-#             if self.command:
-#                 print('click', id(self))
-#                 if not callable(self.command):
-#                     self.click_timer = self.flash_duration  # Reset click timer
-#                     partial(self.command[0])(self.command[1])
-#                 else:
-#                     partial(self.command)()
-#         # Recurse click to check if a child is hovered
-#         else:
-#             for child in self.children:
-#                 child.get_clicked()
-
-#     def draw(self):
-
-#         self.click_timer -= 1
-
-#         if not self.is_active:
-#             return
-
-#         self.hover = self.mouse_over()
-
-#         if self.hover:
-#             if self.click_timer > 0:  # Recently clicked
-#                 self.color = self.click_color
-#             else:
-#                 self.color = self.highlight_color
-#         else:
-#             self.color = self.orig_color
-
-#         pygame.draw.rect(self.screen, self.color,
-#                          (self.x, self.y, self.width, self.height), 0)
-
-#         for child in self.children:
-#             child.draw()
-
-#         if self.text:
-#             font = pygame.font.SysFont(
-#                 'Verdana, Segoe UI, Arial, Playbill, Bauhaus 93, Showcard Gothic', int(self.height * 0.75))
-#             text = font.render(self.text, 1, (0, 0, 0))
-#             self.screen.blit(text, (self.x + (self.width/2 - text.get_width()/2),
-#                                     self.y + (self.height/2 - text.get_height()/2)))
-
-#     def mouse_over(self):
-#         mouse_pos = pygame.mouse.get_pos()
-#         if not pygame.mouse.get_focused():
-#             self.color = self.orig_color
-#             return False
-
-#         if mouse_pos[0] > self.x and mouse_pos[0] < self.x + self.width:
-#             if mouse_pos[1] > self.y and mouse_pos[1] < self.y + self.height:
-#                 #print('mouse over: ', id(self))
-#                 return True
-#         return False
-
-#     def open_menu(self):
-#         # self.close_menu()
-#         Menu.active_menu = [child for child in self.children]
-#         Menu.active_menu.append(self)
-#         for item in Menu.active_menu:
-#             item.is_active = True
-
-#     def close_menu(self):
-#         for item in Menu.active_menu[:-1]:
-#             item.is_active = False
-#         Menu.active_menu = []
